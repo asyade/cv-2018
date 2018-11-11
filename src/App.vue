d27e70ef1bbc0ede60da7b101a03bf355f2d6a35
asyade <acorbeau@student.42.fr>
2018-11-11 03:32:21 +0100

    Awsone

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,20 @@
   <v-app dark>
     <meta name="viewport" content="width=device-width, initial-scale=0.41, maximum-scale=1" />
     <v-content>
-      <HelloWorld/>
+      <Main/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Main from './components/Main'
+import Skills from './components/Skills'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main,
+    Skills
   },
   data () {
     return {
